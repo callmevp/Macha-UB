ac491d6bb9b70e6d2ce0f3933bcaf4afb383efcd
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-08-25 02:23:20 +0530

    MACHA

---
+++ Macha-UB/core/machaub_database/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021 CALLMEVP
+# Part of: Macha-Userbot
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import Config
+
+mongodb = AsyncIOMotorClient(Config.MONGODB_URL)
+nexa_mongodb = mongodb["MACHAUB"]
